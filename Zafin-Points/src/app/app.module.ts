b927b5e91eab46abbb0ef87fbc57a5b575a10a5c
siobhanalexa <93355973+siobhanalexa@users.noreply.github.com>
2021-11-01 20:25:21 -0400

    Update app.module.ts

--- Zafin-Points/src/app/app.module.ts
+++ Zafin-Points/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+
 import { AppComponent } from './app.component';
+import { MainNavbarComponent } from './main-navbar/main-navbar.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
     AppComponent
+    MainNavbarComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgbModule
   ],
   providers: [],
   bootstrap: [AppComponent]
