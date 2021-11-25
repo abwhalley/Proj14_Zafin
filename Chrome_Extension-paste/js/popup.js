c1e1b09d6fed896208545df2f578c44c92b02b45
DuncanMcRae18 <92958179+DuncanMcRae18@users.noreply.github.com>
2021-11-25 17:59:53 -0500

    Add files via upload

--- Chrome_Extension-paste/js/popup.js
+++ Chrome_Extension-paste/js/popup.js
@@ -1,10 +1,8 @@
 document.addEventListener('DOMContentLoaded', function(){
-    console.log("yep");
     document.querySelector('button').addEventListener('click', onclick, false)
-    console.log("yep");
 
     function onclick() {chrome.tabs.query({active: true, currentWindow: true}, ([tab]) => {
-        chrome.tabs.sendMessage(tab.id, "12345abc"
+        chrome.tabs.sendMessage(tab.id, "A3245GH"
 );
       });
     }
